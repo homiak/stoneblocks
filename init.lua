0ab4db4c2a306020a25cb8b89892f31b4bc9c4a9
peter <to.petr@gmail.com>
2024-02-11 21:11:31 +0100

    enable register craft

--- init.lua
+++ init.lua
@@ -3,7 +3,4 @@ local modpath = minetest.get_modpath("stoneblocks")
 dofile(modpath .. "/nodes.lua")
 dofile(modpath .. "/dynamic.lua")
 dofile(modpath .. "/ore.lua")
-
-if minetest.get_modpath("default") then
-    dofile(modpath .. "/crafts.lua")
-end
+dofile(modpath .. "/crafts.lua")
